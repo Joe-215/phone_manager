f0b9cd8d88354062c61f9cfa3a41402af5b5b091
Joe-to-sky-215 <joe.cas2511@gmail.com>
2018-03-08 09:13:46 -0800

    Add swagger documentation

--- src/api/routes/v1/index.js
+++ src/api/routes/v1/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
 
+const swaggerDocument = require('./docs/swagger');
 const numberRoutes = require('./number.route');
 
 const router = express.Router();
@@ -12,7 +14,7 @@ router.get('/status', (req, res) => res.send('OK'));
 /**
  * GET v1/docs
  */
-// router.use('/docs', express.static('docs'));
+router.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 router.use('/number', numberRoutes);
 
--- src/api/routes/v1/number.route.js
+++ src/api/routes/v1/number.route.js
@@ -6,7 +6,57 @@ const { create, query } = require('../../validations/number.validation');
 
 const router = express.Router();
 
+/**
+ *  @swagger
+ *  /number:
+ *    post:
+ *      tags:
+ *        - create
+ *      description: Creates phone number
+ *      parameters:
+ *      - in: "body"
+ *        name: "body"
+ *        required: true
+ *        description: "Phone number object to create"
+ *        schema:
+ *          $ref: "#/definitions/NewPhoneNumber"
+ *      responses:
+ *        400:
+ *          description: Validation error
+ *        409:
+ *          description: Duplicate error
+ *        201:
+ *          description: Phone number object created
+ *          schema:
+ *            $ref: "#/definitions/PhoneNumber"
+ */
 router.route('/').post(validate(create), controller.create);
+
+/**
+ *  @swagger
+ *  /number/query:
+ *    get:
+ *      tags:
+ *        - query
+ *      description: Finds phone numbers
+ *      parameters:
+ *      - in: "query"
+ *        name: "number"
+ *        required: true
+ *        description: "Phone number to search by"
+ *        schema:
+ *          type: string
+ *          pattern: '^\+[1-9]\d{6,14}$'
+ *      responses:
+ *        400:
+ *          description: Validation error
+ *        200:
+ *          description: Phone number object created
+ *          schema:
+ *            type: array
+ *            items:
+ *              $ref: "#/definitions/PhoneNumber"
+ */
 router.route('/query').get(validate(query), controller.query);
 
 module.exports = router;
